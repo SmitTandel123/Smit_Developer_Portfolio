f3da9fc3015506606c98ba8f1e1ed3fbf640ac80
SmitTandel123 <134622676+SmitTandel123@users.noreply.github.com>
2024-07-12 19:01:46 +0530

    Initial commit

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,27 +1,36 @@
 import React from "react";
 import "./Projects.css";
+import Image1 from "../images/pro1.png";
+import Image2 from "../images/pro2.png";
+import Image3 from "../images/pro3.png";
 
 const Projects = () => {
-  
-  <script src="https://unpkg.com/typed.js@2.0.16/dist/typed.umd.js"></script>
-  
+  <script src="https://unpkg.com/typed.js@2.0.16/dist/typed.umd.js"></script>;
+
   return (
     <div className="flex flex-col items-center justify-center py-10">
-      <a href="/" previewlistener="true" className="text-4xl font-semibold text-red-600 dark:text-red-600 mb-8">
-      &lt;Pro<span className="text-red-600">jects/&gt;</span>
-            </a>
+      <a
+        href="/"
+        previewlistener="true"
+        className="text-4xl font-semibold text-red-600 dark:text-red-600 mb-8"
+      >
+        &lt;Pro<span className="text-red-600">jects/&gt;</span>
+      </a>
       <section className="flex flex-wrap justify-center gap-8">
         {/* project1 */}
-        
+
         <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-        
           <a href="/">
-            <img className="rounded-t-lg w-full h-48 object-cover"  src="" alt="" />
+            <img
+              className="rounded-t-lg w-full h-48 object-cover"
+              src={Image1}
+              alt=""
+            />
           </a>
           <div class="p-5">
             <a href="/">
               <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                Noteworthy technology acquisitions 2021
+                TECH CODER 2021
               </h5>
             </a>
             <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
@@ -29,10 +38,10 @@ const Projects = () => {
               far, in reverse chronological order.
             </p>
             <a
-              href="/"
+              href="https://project-1-rmv88sq6c-smit-tandels-projects.vercel.app"
               class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             >
-              Read more
+              Visit
               <svg
                 class="rtl:rotate-180 w-3.5 h-3.5 ms-2"
                 aria-hidden="true"
@@ -53,25 +62,28 @@ const Projects = () => {
         </div>
         {/* project2 */}
         <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-        
-          <a href="/">
-            <img className="rounded-t-lg w-full h-48 object-cover"  src="" alt="" />
+          <a href="https://magenta-mooncake-685e48.netlify.app/">
+            <img
+              className="rounded-t-lg w-full h-48 object-cover"
+              src={Image2}
+              alt=""
+            />
           </a>
           <div class="p-5">
             <a href="/">
               <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                Noteworthy technology acquisitions 2021
+                DREAM DESIGN 2022
               </h5>
             </a>
             <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
-              Here are the biggest enterprise technology acquisitions of 2021 so
+              Here are the biggest enterprise technology acquisitions of 2022 so
               far, in reverse chronological order.
             </p>
             <a
               href="/"
               class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             >
-              Read more
+              Visit
               <svg
                 class="rtl:rotate-180 w-3.5 h-3.5 ms-2"
                 aria-hidden="true"
@@ -92,14 +104,17 @@ const Projects = () => {
         </div>
         {/* project3 */}
         <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-        
           <a href="/">
-            <img className="rounded-t-lg w-full h-48 object-cover"  src="" alt="" />
+            <img
+              className="rounded-t-lg w-full h-48 object-cover"
+              src={Image3}
+              alt=""
+            />
           </a>
           <div class="p-5">
             <a href="/">
               <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                Noteworthy technology acquisitions 2021
+                HOTEL SHUBHAM PALACE
               </h5>
             </a>
             <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
@@ -107,10 +122,10 @@ const Projects = () => {
               far, in reverse chronological order.
             </p>
             <a
-              href="/"
+              href="https://mihirponkshe.github.io/Restaurants/"
               class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             >
-              Read more
+              Visit
               <svg
                 class="rtl:rotate-180 w-3.5 h-3.5 ms-2"
                 aria-hidden="true"
